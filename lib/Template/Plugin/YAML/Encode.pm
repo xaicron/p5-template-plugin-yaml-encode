2e888d68f148fdce89368bb4b54da6ce5aa78e76
unknown <y-shimada@.(none)>
2009-08-24 15:40:34 +0900

    initial commit

---
+++ lib/Template/Plugin/YAML/Encode.pm
@@ -0,0 +1,41 @@
+package Template::Plugin::YAML::Encode;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Template::Plugin::YAML::Encode -
+
+=head1 SYNOPSIS
+
+  use Template::Plugin::YAML::Encode;
+
+=head1 DESCRIPTION
+
+Template::Plugin::YAML::Encode is
+
+=head1 METHODS
+
+=over
+
+=item xxx
+
+=back
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
